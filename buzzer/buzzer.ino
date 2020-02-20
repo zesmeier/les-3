bd907b970df7169a9f8090471f2726615faad797
zesmeier <46023107+zesmeier@users.noreply.github.com>
2020-02-20 16:04:05 +0100

    inleveren

---
+++ buzzer/buzzer.ino
@@ -0,0 +1,15 @@
+/* Arduino tutorial - Buzzer / Piezo Speaker
+   More info and circuit: http://www.ardumotive.com/how-to-use-a-buzzer-en.html
+   Dev: Michalis Vasilakis // Date: 9/6/2015 // www.ardumotive.com */
+
+const int buzzer = 9; //buzzer to arduino pin 9
+
+void setup(){
+  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
+}
+
+void loop(){
+  tone(buzzer, 400); // Send 10KHz sound signal...
+  delay(1000);        // ...for 16 sec
+  delay(1000);        // ...for 1sec
+}
